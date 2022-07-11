6a2b48e50513c641fb8dbe363f2668f9250bd7a3
MKBenjie <mugaggabenjie@gmail.com>
2022-07-11 14:26:15 +0300

    more changes

--- the_library/settings.py
+++ the_library/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-xw$j8m7mys5&ub+4_s6f3p!2490vqa1nxvhrm4-9()xxk)=e-9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','gitlab.com/MKBenjie/my-lib.git']
+ALLOWED_HOSTS = ['127.0.0.1:8000','my-lib-benjie.herokuapp.com/']
 
 
 # Application definition
